0ff0a77511e2c7956da42eda1b020cddfb87c668
Ioannis Papakonstantopoulos <ioannis.papakonstantopoulos@eurodyn.com>
2019-09-26 15:02:23 +0300

    Upgraded phone-list, phone-detail , checkmark, phone service.

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,15 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { UpgradeModule } from '@angular/upgrade/static';
 
+import { HttpClientModule } from '@angular/common/http';
+
+import { Phone } from './core/phone/phone.service';
+
+import { FormsModule } from '@angular/forms';
+import { PhoneListComponent } from './phone-list/phone-list.component';
+import { routeParamsProvider } from './ajs-upgraded-providers';
+import { PhoneDetailComponent } from './phone-detail/phone-detail.component';
+import { CheckmarkPipe } from './core/checkmark/checkmark.pipe';
+
 @NgModule({
   imports: [
     BrowserModule,
     UpgradeModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  declarations: [
+    PhoneListComponent,
+    PhoneDetailComponent,
+    CheckmarkPipe,
+  ],
+  entryComponents: [
+    PhoneListComponent,
+    PhoneDetailComponent  
+  ],
+  providers: [
+    Phone,
+    routeParamsProvider
   ],
 })
 export class AppModule {
-    constructor(private upgrade: UpgradeModule) { }
-    ngDoBootstrap() {
-        this.upgrade.bootstrap(document.documentElement, ['phonecatApp']);
+  constructor(private upgrade: UpgradeModule) { }
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.documentElement, ['phonecatApp']);
   }
 }
--- app/core/phone/phone.service.ts
+++ app/core/phone/phone.service.ts
@@ -1,14 +1,44 @@
 
-angular.
-  module('core.phone').
-  factory('Phone', ['$resource',
-    function($resource: angular.resource.IResourceService) {
-      return $resource('phones/:phoneId.json', {}, {
-        query: {
-          method: 'GET',
-          params: {phoneId: 'phones'},
-          isArray: true
-        }
-      });
-    }
-  ]);
+// angular.
+//   module('core.phone').
+//   factory('Phone', ['$resource',
+//     function($resource: angular.resource.IResourceService) {
+//       return $resource('phones/:phoneId.json', {}, {
+//         query: {
+//           method: 'GET',
+//           params: {phoneId: 'phones'},
+//           isArray: true
+//         }
+//       });
+//     }
+//   ]);
+
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+declare var angular: angular.IAngularStatic;
+import { downgradeInjectable } from '@angular/upgrade/static';
+
+export interface PhoneData {
+  name: string;
+  snippet: string;
+  images: string[];
+}
+
+@Injectable()
+export class Phone {
+  constructor(private http: HttpClient) { console.log("Inside Phone sercice constructor"); }
+  query(): Observable<PhoneData[]> {
+    console.log("Querying for all phones.");
+    return this.http.get<PhoneData[]>(`phones/phones.json`);
+  }
+  get(id: string): Observable<PhoneData> {
+    console.log("Querying for phone ID:" + id);
+    return this.http.get<PhoneData>(`phones/${id}.json`);
+  }
+}
+
+angular.module('core.phone')
+  .factory('phone', downgradeInjectable(Phone));
